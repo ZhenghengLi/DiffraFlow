e876bfacf29971c8c578dfd96996969361507dfd
Zhengheng Li <zhenghenge@gmail.com>
2019-10-14 16:45:06 +0800

    add subproject 'testing' to do manual unit test

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,10 +3,14 @@
 tmp_dir=$(dirname ${BASH_SOURCE[0]})
 my_dir=$(cd $tmp_dir/.. > /dev/null ; pwd)
 
-# dispatcher
 export CLASSPATH=
+
+# dispatcher
 export CLASSPATH=$CLASSPATH:$my_dir/dispatcher/build/libs/*
 
 # combiner
 export PATH=combiner/build/install/main/release:$PATH
+
+# testing
+export CLASSPATH=$CLASSPATH:$my_dir/testing/build/libs/*
 
---
+++ testing/src/main/java/shine/CombinerConsoleTest.java
@@ -0,0 +1,5 @@
+package shine;
+
+public class CombinerConsoleTest {
+
+}

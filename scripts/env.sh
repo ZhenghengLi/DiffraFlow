70b5a33ec09b6d5bedf8eaa3e2f5ede1b4d6f86d
Zhengheng Li <zhenghenge@gmail.com>
2019-10-24 12:17:00 +0800

    update build.gradle and scripts/env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -6,17 +6,21 @@ my_dir=$(cd $tmp_dir/.. > /dev/null ; pwd)
 export CLASSPATH=
 
 # dispatcher
+echo "Project 'dispatcher'"
 export CLASSPATH=$CLASSPATH:$my_dir/dispatcher/build/libs/*
 
-# combiner
-export PATH=$my_dir/combiner/build/install/main/release:$PATH
-
-# ingester
-export PATH=$my_dir/ingester/build/install/main/release:$PATH
-
-# monitor
-export PATH=$my_dir/monitor/build/install/main/release:$PATH
-
 # testing
+echo "Project 'testing'"
 export CLASSPATH=$CLASSPATH:$my_dir/testing/build/libs/*
+
+# cpp-application
+for name in combiner ingester monitor
+do
+    echo "Project '$name'"
+    export PATH=$my_dir/$name/build/exe/main/release:$PATH
+done
+
+# cpp-library
+echo "Project 'common'"
+export LD_LIBRARY_PATH=$my_dir/common/build/lib/main/release/shared:$LD_LIBRARY_PATH
 

5a87f2cb50b016d0819505d762ad3cbf74f007fb
Zhengheng Li <zhenghenge@gmail.com>
2019-10-24 08:37:16 +0800

    add some code to ImageData

--- combiner/src/main/cpp/ImageData.cpp
+++ combiner/src/main/cpp/ImageData.cpp
@@ -1,9 +1,35 @@
 #include "ImageData.hpp"
+#include "ImageFrame.hpp"
 
 shine::ImageData::ImageData() {
-
+    imgFrm_len_ = 1;
+    imgFrm_arr_ = new ImageFrame*[imgFrm_len_];
+    for (size_t i = 0; i < imgFrm_len_; i++) {
+        imgFrm_arr_[i] = nullptr;
+    }
 }
 
 shine::ImageData::~ImageData() {
+    for (size_t i = 0; i < imgFrm_len_; i++) {
+        if (imgFrm_arr_[i] != nullptr) {
+            delete imgFrm_arr_[i];
+        }
+    }
+    delete [] imgFrm_arr_;
+}
 
+bool shine::ImageData::put_imgfrm(size_t index, const ImageFrame& imgfrm) {
+    if (index >= imgFrm_len_) return false;
+    imgFrm_arr_[index] = new ImageFrame(imgfrm);
+    return true;
+}
+
+bool shine::ImageData::serialize(char* data, size_t len) {
+
+    return true;
+}
+
+bool shine::ImageData::deserialize(char* data, size_t len) {
+
+    return true;
 }
--- combiner/src/main/headers/ImageData.hpp
+++ combiner/src/main/headers/ImageData.hpp
@@ -1,11 +1,23 @@
 #ifndef ImageData_H
 #define ImageData_H
 
+#include "Decoder.hpp"
+
 namespace shine {
-    class ImageData {
+
+    class ImageFrame;
+
+    class ImageData: private Decoder {
+    private:
+        size_t imgFrm_len_;
+        ImageFrame** imgFrm_arr_;
+
     public:
         ImageData();
         ~ImageData();
+        bool put_imgfrm(size_t index, const ImageFrame& imgfrm);
+        bool serialize(char* data, size_t len);
+        bool deserialize(char* data, size_t len);
 
     };
 }

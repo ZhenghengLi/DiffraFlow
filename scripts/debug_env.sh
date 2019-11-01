9b09f10dbdae5712621f392170de9cec0a4a266c
Zhengheng Li <zhenghenge@gmail.com>
2019-11-01 11:08:16 +0800

    scripts/debug_env.sh

--- scripts/debug_env.sh
+++ scripts/debug_env.sh
@@ -5,23 +5,26 @@ my_dir=$(cd $tmp_dir/.. > /dev/null ; pwd)
 
 export CLASSPATH=
 
-# dispatcher
-echo "Project 'dispatcher'"
-export CLASSPATH=$CLASSPATH:$my_dir/dispatcher/build/libs/*
-
-# testing
-echo "Project 'testing'"
-export CLASSPATH=$CLASSPATH:$my_dir/testing/build/libs/*
+# java
+for name in dispatcher
+do
+    echo "Project 'diffraflow/$name'"
+    export CLASSPATH=$CLASSPATH:$my_dir/diffraflow/$name/build/libs/*
+done
 
 # cpp-application
 for name in combiner ingester monitor
 do
-    echo "Project '$name'"
-    export PATH=$my_dir/$name/build/exe/main/release:$PATH
+    echo "Project 'diffraflow/$name'"
+    export PATH=$my_dir/diffraflow/$name/build/exe/main/release:$PATH
 done
 
 # cpp-library
 echo "Project 'common'"
-export LD_LIBRARY_PATH=$my_dir/common/build/lib/main/release:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=$my_dir/common/build/lib/main/release:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$my_dir/diffraflow/common/build/lib/main/release:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$my_dir/diffraflow/common/build/lib/main/release:$DYLD_LIBRARY_PATH
+
+# testing
+echo "Project 'testing'"
+export CLASSPATH=$CLASSPATH:$my_dir/testing/build/libs/*
 

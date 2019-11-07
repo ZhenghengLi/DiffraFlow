dc64f586226c8c6d7dbc737d58045167c8cc98b8
Zhengheng Li <zhenghenge@gmail.com>
2019-11-07 11:06:20 +0800

    combiner: start ImageDataServer

--- diffraflow/combiner/src/main/cpp/ImageDataServer.cc
+++ diffraflow/combiner/src/main/cpp/ImageDataServer.cc
@@ -1,7 +1,10 @@
 #include "ImageDataServer.hh"
+#include "ImageCache.hh"
+#include <cassert>
 
-shine::ImageDataServer::ImageDataServer() {
-
+shine::ImageDataServer::ImageDataServer(ImageCache* img_cache) {
+    assert(img_cache != nullptr);
+    image_cache_ = img_cache;
 }
 
 shine::ImageDataServer::~ImageDataServer() {
--- diffraflow/combiner/src/main/headers/ImageDataServer.hh
+++ diffraflow/combiner/src/main/headers/ImageDataServer.hh
@@ -1,10 +1,32 @@
 #ifndef ImageDataServer_H
 #define ImageDataServer_H
 
+#include <thread>
+#include <vector>
+#include <list>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+using std::thread;
+using std::vector;
+using std::list;
+using std::pair;
+using std::atomic;
+using std::mutex;
+using std::condition_variable;
+
 namespace shine {
+
+    class ImageFrameConnection;
+    class ImageCache;
+
     class ImageDataServer {
+    private:
+        ImageCache* image_cache_;
+
     public:
-        ImageDataServer();
+        ImageDataServer(ImageCache* img_cache);
         ~ImageDataServer();
 
     };

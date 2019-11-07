20a2f9460d4d82a253c1254282ea2af65d61a0a1
Zhengheng Li <zhenghenge@gmail.com>
2019-11-07 09:20:33 +0800

    BlockingQueue: add try_offer and try_get

--- diffraflow/common/src/main/public/BlockingQueue.hh
+++ diffraflow/common/src/main/public/BlockingQueue.hh
@@ -13,6 +13,8 @@ using std::mutex;
 using std::lock_guard;
 using std::unique_lock;
 using std::condition_variable;
+using std::atomic_bool;
+using std::atomic;
 
 namespace shine {
     template <typename E>
@@ -22,24 +24,38 @@ namespace shine {
         condition_variable cv_push_;
         condition_variable cv_take_;
         queue<E>* internal_queue_;
-        bool aborted_;
-        size_t max_size_;
+        atomic_bool aborted_;
+        atomic<size_t> max_size_;
     public:
         BlockingQueue(size_t ms = 100);
         ~BlockingQueue();
 
         // synchronized methods
         void set_maxsize(size_t ms);
+        size_t maxsize();
+
         size_t size();
-        bool   empty();
-        bool   push(const E& el);
-        bool   push(const E& el, int timeout);
-        bool   take(E& el);
-        bool   take(E& el, int timeout);
-        bool   offer(const E& el);
-        bool   get(E& el);
-        void   abort();
-        bool   aborted();
+        bool empty();
+
+        // blocking push until have space, return false when aborted
+        bool push(const E& el);
+        // blocking push with timeout, return false when timeout or aborted
+        bool push(const E& el, int timeout);
+        // blocking take until have element, return false when aborted
+        bool take(E& el);
+        // blocking take with timeout, return false when timeout or aborted
+        bool take(E& el, int timeout);
+        // push only when have space now, otherwise directly return false
+        bool offer(const E& el);
+        // take only when have element now, otherwise directly return false
+        bool get(E& el);
+        // non-blocking push only when get lock and have space
+        bool try_offer(const E& el);
+        // non-blocking take only when get lock and have element
+        bool try_get(E& el);
+
+        void abort();
+        bool aborted();
     };
 
     template <typename E>
@@ -58,6 +74,11 @@ namespace shine {
     template <typename E>
     void BlockingQueue<E>::set_maxsize(size_t ms) {
         max_size_ = ms;
+    }
+
+    template <typename E>
+    size_t BlockingQueue<E>::maxsize() {
+        return max_size_;
     }
 
     template <typename E>
@@ -157,8 +178,31 @@ namespace shine {
     }
 
     template <typename E>
+    bool BlockingQueue<E>::try_offer(const E& el) {
+        unique_lock<mutex> lk(mtx_, std::try_to_lock);
+        if (!lk.owns_lock()) return false;
+        if (internal_queue_->size() < max_size_) {
+            internal_queue_->push(el);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    template <typename E>
+    bool BlockingQueue<E>::try_get(E& el) {
+        unique_lock<mutex> lk(mtx_, std::try_to_lock);
+        if (!lk.owns_lock()) return false;
+        if (internal_queue_->empty()) {
+            return false;
+        } else {
+            el = internal_queue_->front();
+            internal_queue_->pop();
+        }
+    }
+
+    template <typename E>
     void BlockingQueue<E>::abort() {
-        lock_guard<mutex> lk(mtx_);
         aborted_ = true;
         cv_push_.notify_all();
         cv_take_.notify_all();
@@ -166,7 +210,6 @@ namespace shine {
 
     template <typename E>
     bool BlockingQueue<E>::aborted() {
-        lock_guard<mutex> lk(mtx_);
         return aborted_;
     }
 

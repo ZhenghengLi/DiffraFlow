bc7a7d79e399a49f1b38194ecad623b2ff0b7bc2
Zhengheng Li <zhenghenge@gmail.com>
2020-01-16 18:19:01 +0800

    ingester

--- diffraflow/ingester/src/main/cpp/IngMain.cc
+++ diffraflow/ingester/src/main/cpp/IngMain.cc
@@ -8,8 +8,8 @@ using namespace diffraflow;
 int main(int argc, char** argv) {
     cout << "I am ingester." << endl;
 
-    ImageData img_data;
-    img_data.serialize(nullptr, 0);
+    // ImageData img_data;
+    // img_data.serialize(nullptr, 0);
 
     return 0;
 }

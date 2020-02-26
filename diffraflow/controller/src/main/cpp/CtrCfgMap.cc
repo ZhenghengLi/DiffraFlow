ea559789c58ecfb3780626dd3cac9aec6cd83028
Zhengheng Li <zhenghenge@gmail.com>
2020-02-26 12:11:41 +0800

    add class CtrCfgMap

---
+++ diffraflow/controller/src/main/cpp/CtrCfgMap.cc
@@ -0,0 +1,18 @@
+#include "CtrCfgMap.hh"
+
+diffraflow::CtrCfgMap::CtrCfgMap() {
+
+}
+
+diffraflow::CtrCfgMap::~CtrCfgMap() {
+
+}
+
+bool diffraflow::CtrCfgMap::load(const char* filename) {
+
+    return true;
+}
+
+void diffraflow::CtrCfgMap::print() {
+
+}
---
+++ diffraflow/controller/src/main/headers/CtrCfgMap.hh
@@ -0,0 +1,30 @@
+#ifndef __CtrCfgMap_H__
+#define __CtrCfgMap_H__
+
+#include <map>
+#include <string>
+#include <log4cxx/logger.h>
+
+#include "GenericConfiguration.hh"
+
+using std::map;
+using std::string;
+
+namespace diffraflow {
+    class CtrCfgMap: public GenericConfiguration {
+    public:
+        CtrCfgMap();
+        ~CtrCfgMap();
+
+        bool load(const char* filename) override;
+        void print() override;
+
+    public:
+        map<string, string> data;
+
+    private:
+        static log4cxx::LoggerPtr logger_;
+    };
+}
+
+#endif

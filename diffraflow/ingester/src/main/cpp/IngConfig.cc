767ee6fcb76452874fbf015ad4cd28fa12c91427
Zhengheng Li <zhenghenge@gmail.com>
2020-02-28 16:55:11 +0800

    IngConfig::convert_and_check_()

--- diffraflow/ingester/src/main/cpp/IngConfig.cc
+++ diffraflow/ingester/src/main/cpp/IngConfig.cc
@@ -2,6 +2,7 @@
 #include <log4cxx/logger.h>
 #include <log4cxx/ndc.h>
 #include <iostream>
+#include <ctime>
 
 using std::cout;
 using std::endl;
@@ -11,8 +12,10 @@ log4cxx::LoggerPtr diffraflow::IngConfig::logger_
 
 diffraflow::IngConfig::IngConfig() {
     ingester_id = -1;
-    combiner_host = "localhost";
     combiner_port = -1;
+
+    combiner_host = "localhost";
+    conf_map_in_use["combiner_host"] = "localhost";
 
 }
 
@@ -68,31 +71,69 @@ bool diffraflow::IngConfig::load(const char* filename) {
         succ_flag = false;
     }
 
+    conf_map_mtime_ = time(NULL);
+    conf_map_in_use["config_mtime"] = string(ctime(&conf_map_mtime_));
+
     return succ_flag;
 }
 
 void diffraflow::IngConfig::print() {
-
+    zookeeper_print_setting();
+    cout << "server config:" << endl;
+    cout << "- ingester_id = " << ingester_id << endl;
+    cout << "- combiner_host = " << combiner_host << endl;
+    cout << "- combiner_port = " << combiner_port << endl;
+    cout << "dynamic parameters:" << endl;
+    cout << "- dy_param_int = " << dy_param_int << endl;
+    cout << "- dy_param_double = " << dy_param_double << endl;
+    cout << "- dy_param_string = " << dy_param_string << endl;
 }
 
 void diffraflow::IngConfig::convert_and_check_() {
+    time_t now_time = time(NULL);
+    cout << "configuration updating (" << ctime(&now_time)
+         << "): received new config map with mtime " << ctime(&conf_map_mtime_) << endl;
     for (map<string, string>::iterator iter = conf_map_.begin(); iter != conf_map_.end(); ++iter) {
         if (iter->first == "dy_param_int") {
-            int value = atoi(iter->second.c_str());
-            if (value > 10) {
-                dy_param_int = value;
+            int new_value = atoi(iter->second.c_str());
+            if (new_value > 10) {
+                int old_value = dy_param_int;
+                dy_param_int = new_value;
                 conf_map_in_use[iter->first] = iter->second;
                 iter = conf_map_.erase(iter);
+                cout << "configuration updated (" << ctime(&now_time)
+                     << "): dy_param_int " << old_value << " -> " << new_value << endl;
+            } else {
+                cout << "configuration updating (" << ctime(&now_time)
+                     << "): dy_param_int " << new_value << " is out of range." << endl;
             }
         } else if (iter->first == "dy_param_double") {
-            dy_param_double = atof(iter->second.c_str());
-            conf_map_in_use[iter->first] = iter->second;
-            iter = conf_map_.erase(iter);
+            double new_value = atof(iter->second.c_str());
+            if (new_value < 1000) {
+                double old_value = dy_param_double;
+                dy_param_double = new_value;
+                conf_map_in_use[iter->first] = iter->second;
+                iter = conf_map_.erase(iter);
+                cout << "configuration updated (" << ctime(&now_time)
+                     << "): dy_param_double " << old_value << " -> " << new_value << endl;
+            } else {
+                cout << "configuration updating (" << ctime(&now_time)
+                     << "): dy_param_double " << new_value << " is out of range." << endl;
+            }
         } else if (iter->first == "dy_param_string") {
-            dy_param_string = iter->second;
-            conf_map_in_use[iter->first] = iter->second;
-            iter = conf_map_.erase(iter);
+            string new_value = iter->second;
+            if (new_value.length() < 3) {
+                string old_value = dy_param_string;
+                dy_param_string = new_value;
+                conf_map_in_use[iter->first] = iter->second;
+                iter = conf_map_.erase(iter);
+                cout << "configuration updated (" << ctime(&now_time)
+                     << "): dy_param_string \"" << old_value << "\" -> \"" << new_value << "\"" << endl;
+            } else {
+                cout << "configuration updating (" << ctime(&now_time)
+                     << "): dy_param_string " << new_value << " is too short." << endl;
+            }
         }
-
     }
+    conf_map_in_use["config_mtime"] = string(ctime(&conf_map_mtime_));
 }
--- diffraflow/ingester/src/main/cpp/IngMain.cc
+++ diffraflow/ingester/src/main/cpp/IngMain.cc
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <cstdlib>
+#include <csignal>
+#include <cstring>
+#include <log4cxx/propertyconfigurator.h>
+#include <log4cxx/patternlayout.h>
+#include <log4cxx/consoleappender.h>
+#include <log4cxx/logmanager.h>
+#include <log4cxx/logstring.h>
 
 #include "IngOptMan.hh"
+#include "IngConfig.hh"
 
 using namespace std;
 using namespace diffraflow;
+
+IngConfig* gConfiguration = nullptr;
+
+void clean(int signum) {
+    cout << "do cleaning ..." << endl;
+    if (gConfiguration != nullptr) {
+        delete gConfiguration;
+        gConfiguration = nullptr;
+    }
+    exit(0);
+}
+
+void init(IngConfig* config_obj) {
+    // register signal action
+    struct sigaction action;
+    // for Ctrl-C
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = &clean;
+    if (sigaction(SIGINT, &action, nullptr)) {
+        perror("sigaction() failed.");
+        exit(1);
+    }
+    // ignore SIGPIPE
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = SIG_IGN;
+    if (sigaction(SIGPIPE, &action, nullptr)) {
+        perror("sigaction() failed.");
+        exit(1);
+    }
+}
 
 int main(int argc, char** argv) {
     // process command line parameters
@@ -12,6 +51,28 @@ int main(int argc, char** argv) {
         option_man.print();
         return 2;
     }
+
+    // configure logger
+    if (option_man.logconf_file.empty()) {
+        log4cxx::LogManager::getLoggerRepository()->setConfigured(true);
+        static const log4cxx::LogString logfmt(LOG4CXX_STR("%d [%t] %-5p %c - %m%n"));
+        log4cxx::LayoutPtr layout(new log4cxx::PatternLayout(logfmt));
+        log4cxx::AppenderPtr appender(new log4cxx::ConsoleAppender(layout));
+        log4cxx::Logger::getRootLogger()->addAppender(appender);
+        log4cxx::Logger::getRootLogger()->setLevel(log4cxx::Level::getDebug());
+    } else {
+        log4cxx::PropertyConfigurator::configure(option_man.logconf_file);
+    }
+    // parse configuration file
+    gConfiguration = new IngConfig();
+    if (!gConfiguration->load(option_man.config_file.c_str())) {
+        cout << "Failed to load configuration file: " << option_man.config_file << endl;
+        return 1;
+    }
+    gConfiguration->print();
+    cout << "start to sync configurations with zookeeper ..." << endl;
+    gConfiguration->zookeeper_sync_config();
+
 
 
     return 0;

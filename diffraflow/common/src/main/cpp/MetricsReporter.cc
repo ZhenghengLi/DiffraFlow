068821726081e41f4d3be69c06f3382e020271a7
Zhengheng Li <zhenghenge@gmail.com>
2020-03-24 10:16:40 +0800

    MetricsReporter::aggregate_metrics_()

--- diffraflow/common/src/main/cpp/MetricsReporter.cc
+++ diffraflow/common/src/main/cpp/MetricsReporter.cc
@@ -25,3 +25,28 @@ void diffraflow::MetricsReporter::clear() {
     metrics_scalar_.clear();
     metrics_array_.clear();
 }
+
+json::value diffraflow::MetricsReporter::aggregate_metrics_() {
+    json::value root_json;
+    for (map<string, MetricsProvider*>::iterator iter = metrics_scalar_.begin();
+        iter != metrics_scalar_.end(); ++iter) {
+        root_json[iter->first] = iter->second->collect_metrics();
+    }
+    for (map<string, vector<MetricsProvider*> >::iterator iter = metrics_array_.begin();
+        iter != metrics_array_.end(); ++iter) {
+        for (size_t i = 0 ; i < iter->second.size(); i++) {
+            root_json[iter->first][i] = iter->second[i]->collect_metrics();
+        }
+    }
+
+}
+
+bool diffraflow::MetricsReporter::start_msg_producer(const char* broker_address, const char* topic) {
+
+    return true;
+}
+
+bool diffraflow::MetricsReporter::start_http_server(const char* host, int port) {
+
+    return true;
+}
--- diffraflow/common/src/main/public/MetricsReporter.hh
+++ diffraflow/common/src/main/public/MetricsReporter.hh
@@ -16,10 +16,16 @@ namespace diffraflow {
         MetricsReporter();
         ~MetricsReporter();
 
+        bool start_msg_producer(const char* broker_address, const char* topic);
+        bool start_http_server(const char* host, int port);
+
         void add(string name, MetricsProvider* mp_obj);
         void add(string name, vector<MetricsProvider*>& mp_obj_vec);
         void add(string name, MetricsProvider** mp_obj_arr, size_t len);
         void clear();
+
+    private:
+        json::value aggregate_metrics_();
 
     private:
         map<string, MetricsProvider*> metrics_scalar_;

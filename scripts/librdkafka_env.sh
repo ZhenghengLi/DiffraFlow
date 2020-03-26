db64a1faab132a16b5572a4ba40bc3835f729aa8
Zhengheng Li <zhenghenge@gmail.com>
2020-03-26 15:41:33 +0800

    remove librdkafka_env.sh

--- scripts/librdkafka_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-base_dir=/opt/mark/CppLibs/librdkafka
-
-export CPATH=$base_dir/include:$CPATH
-export LIBRARY_PATH=$base_dir/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$base_dir/lib:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=$base_dir/lib:$DYLD_LIBRARY_PATH
-

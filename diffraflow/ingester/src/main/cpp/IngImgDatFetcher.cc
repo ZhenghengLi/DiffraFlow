c70cb8e9adfe307c8eb721de0fc26598e64c40f7
Zhengheng Li <zhenghenge@gmail.com>
2020-04-03 12:34:46 +0800

    IngImgDatFetcher::connect_to_combiner()

--- diffraflow/ingester/src/main/cpp/IngImgDatFetcher.cc
+++ diffraflow/ingester/src/main/cpp/IngImgDatFetcher.cc
@@ -15,12 +15,46 @@ diffraflow::IngImgDatFetcher::~IngImgDatFetcher() {
 
 }
 
-void diffraflow::IngImgDatFetcher::set_recnxn_policy(int wait_time, int max_count) {
+void diffraflow::IngImgDatFetcher::set_recnxn_policy(size_t wait_time, size_t max_count) {
     recnxn_wait_time_ = wait_time;
     recnxn_max_count_ = max_count;
 }
 
 bool diffraflow::IngImgDatFetcher::connect_to_combiner() {
-
-    return true;
+    if (stopped_) {
+        LOG4CXX_WARN(logger_, "image data fetcher is stopped, abort connecting.");
+        return false;
+    }
+    bool result = false;
+    for (size_t current_count = 0; current_count <= recnxn_max_count_; current_count++) {
+        if (connect_to_server()) {
+            result = true;
+            break;
+        } else {
+            if (current_count < recnxn_max_count_) {
+                LOG4CXX_WARN(logger_, "failed to connect to combiner "
+                    << dest_host_ << ":" << dest_port_
+                    << ", wait for " << recnxn_wait_time_ << "ms and retry ("
+                    << current_count + 1 << "/" << recnxn_max_count_ << ") ...");
+                if (recnxn_wait_time_ > 0) {
+                    unique_lock<mutex> ulk(cnxn_mtx_);
+                    cnxn_cv_.wait_for(ulk, std::chrono::milliseconds(recnxn_wait_time_),
+                        [this]() {return stopped_.load();}
+                    );
+                }
+                if (stopped_.load()) {
+                    LOG4CXX_WARN(logger_, "image data fetcher is stopped, abort reconnecting.");
+                    return false;
+                }
+            }
+        }
+    }
+    if (result) {
+        LOG4CXX_INFO(logger_, "successfully connected to combiner " << dest_host_ << ":" << dest_port_);
+        return true;
+    } else {
+        LOG4CXX_WARN(logger_, "failed to connect to combiner " << dest_host_ << ":" << dest_port_
+            << " after " << recnxn_max_count_ << "retry counts.");
+        return false;
+    }
 }
--- diffraflow/ingester/src/main/headers/IngImgDatFetcher.hh
+++ diffraflow/ingester/src/main/headers/IngImgDatFetcher.hh
@@ -4,7 +4,14 @@
 #include "GenericClient.hh"
 #include "IngImgDatRawQueue.hh"
 
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
 #include <log4cxx/logger.h>
+
+using std::mutex;
+using std::condition_variable;
+using std::atomic_bool;
 
 namespace diffraflow {
     class IngImgDatFetcher: public GenericClient {
@@ -13,14 +20,20 @@ namespace diffraflow {
             uint32_t ingester_id, IngImgDatRawQueue* raw_queue);
         ~IngImgDatFetcher();
 
-        void set_recnxn_policy(int wait_time, int max_count);
+        void set_recnxn_policy(size_t wait_time, size_t max_count);
         bool connect_to_combiner();
 
+        bool start();
+
     private:
-        int recnxn_wait_time_;
-        int recnxn_max_count_;
+        size_t recnxn_wait_time_;
+        size_t recnxn_max_count_;
 
         IngImgDatRawQueue* imgdat_raw_queue_;
+
+        atomic_bool stopped_;
+        mutex cnxn_mtx_;
+        condition_variable cnxn_cv_;
 
     private:
         static log4cxx::LoggerPtr logger_;

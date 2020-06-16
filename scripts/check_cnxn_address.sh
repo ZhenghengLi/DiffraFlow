7592ed18a7689e9d24bcda78f5bb0dbec7ddd36c
Zhengheng Li <zhenghenge@gmail.com>
2020-06-16 15:55:09 +0800

    update check_cnxn_address.sh

--- scripts/check_cnxn_address.sh
+++ scripts/check_cnxn_address.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 if [[ -z $1 ]]; then
-    echo "USAGE: check_connection.sh address_list.txt"
+    echo "USAGE: check_connection.sh <host1:port1> <host2:port2> ..."
     exit 1
 fi
 
 function check_cnxn_address {
-    IFS=: read -r host port <<< $1
-    if [[ -z $host || -z $port ]]; then
-        return 1
-    fi
-    if ! nc -z -v -w 1 $host $port; then
-        return 1
-    fi
+    for item in $@; do
+        IFS=: read -r host port <<< $item
+        if [[ -z $host || -z $port ]]; then
+            return 1
+        fi
+        if ! nc -z -v -w 1 $host $port; then
+            return 1
+        fi
+    done
 }
 
-until check_cnxn_address $1; do
+until check_cnxn_address $@; do
     sleep ${2:-1}
     echo "continue checking ..."
 done

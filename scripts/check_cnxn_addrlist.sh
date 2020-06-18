573c016e03e09c150363568adf8f336b2eef6959
Zhengheng Li <zhenghenge@gmail.com>
2020-06-18 14:10:55 +0800

    check_cnxn_addrlist.sh: can check http url now

--- scripts/check_cnxn_addrlist.sh
+++ scripts/check_cnxn_addrlist.sh
@@ -7,9 +7,21 @@ fi
 
 function check_cnxn_addrlist {
     while read -r line; do
+        # skip empty line and comment
         if [[ -z $line || $line =~ ^# ]]; then
             continue
         fi
+        # remove http:// or https:// header
+        if [[ $line =~ ^\s*https?:// ]]; then
+            line=$(sed -r "s|^\s*https?://||" <<< $line)
+        fi
+        # replace NODE_NAME or NODE_IP
+        if [[ $line =~ NODE_NAME && ! -z $NODE_NAME ]]; then
+            line=$(sed -r "s|NODE_NAME|$NODE_NAME|" <<< $line)
+        elif [[ $line =~ NODE_IP && ! -z $NODE_IP ]]; then
+            line=$(sed -r "s|NODE_IP|$NODE_IP|" <<< $line)
+        fi
+        # extract host and port, then check TCP connection
         IFS=: read -r host port <<< $line
         if [[ -z $host || -z $port ]]; then
             return 1
@@ -21,6 +33,6 @@ function check_cnxn_addrlist {
 }
 
 until check_cnxn_addrlist $1; do
-    sleep ${2:-1}
+    sleep 1
     echo "continue checking ..."
 done

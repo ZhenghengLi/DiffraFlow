3bddc0c8f506395650e4b7fde69689defc362504
Zhengheng Li <zhenghenge@gmail.com>
2020-07-08 17:00:28 +0800

    update build.gradle and debug_env.sh

--- scripts/debug_env.sh
+++ scripts/debug_env.sh
@@ -5,10 +5,11 @@ my_dir=$(cd $tmp_dir/.. > /dev/null ; pwd)
 
 # change this variable if needed
 packageDir=$my_dir/build/package_debug
+scriptsDir=$my_dir/scripts
 
 export CLASSPATH=$packageDir/jar/*
 
-export PATH=$packageDir/bin:$PATH
+export PATH=$scriptsDir:$packageDir/bin:$PATH
 
 export LD_LIBRARY_PATH=$packageDir/lib:$LD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=$packageDir/lib:$DYLD_LIBRARY_PATH

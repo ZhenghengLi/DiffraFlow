3afbdb2cbd4b5b75a0d124b4343f021ea8ddc184
Zhengheng Li <zhenghenge@gmail.com>
2020-08-14 12:32:11 +0800

    combiner: test serialization only

--- diffraflow/combiner/src/main/cpp/CmbImgDatConn.cc
+++ diffraflow/combiner/src/main/cpp/CmbImgDatConn.cc
@@ -35,6 +35,10 @@ bool diffraflow::CmbImgDatConn::do_preparing_and_sending_() {
     // serialize image data
     image_buffer_.clear();
     msgpack::pack(image_buffer_, *one_image);
+
+    // debug
+    return true;
+
     // serialize head
     char head_buffer[4];
     gPS.serializeValue<uint32_t>(0xABCDEEEE, head_buffer, 4);

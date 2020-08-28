dbfa8b61bbeab9af0aba7639528165a63ea16b9e
Zhengheng Li <zhenghenge@gmail.com>
2020-08-28 12:10:41 +0800

    add class ImageDataType

---
+++ diffraflow/common/src/main/cpp/ImageDataType.cc
@@ -0,0 +1,104 @@
+#include "ImageDataType.hh"
+#include "Decoder.hh"
+
+diffraflow::ImageDataType::ImageDataType() : H5::CompType(sizeof(Field)) {
+    //// bunch_id
+    insertMember("bunch_id", HOFFSET(Field, bunch_id), H5::PredType::NATIVE_UINT64);
+    //// alignment
+    hsize_t module_dim[] = {MOD_CNT};
+    H5::ArrayType alignment_t(H5::PredType::NATIVE_HBOOL, 1, module_dim);
+    insertMember("alignment", HOFFSET(Field, alignment), alignment_t);
+    //// cell_id
+    H5::ArrayType cell_id_t(H5::PredType::NATIVE_INT16, 1, module_dim);
+    insertMember("cell_id", HOFFSET(Field, cell_id), cell_id_t);
+    //// status
+    H5::ArrayType status_t(H5::PredType::NATIVE_UINT16, 1, module_dim);
+    insertMember("status", HOFFSET(Field, status), status_t);
+    //// image_frame
+    hsize_t frame_dim[] = {MOD_CNT, FRAME_H, FRAME_W};
+    H5::ArrayType pixel_data_t(H5::PredType::NATIVE_FLOAT, 3, frame_dim);
+    insertMember("pixel_data", HOFFSET(Field, pixel_data), pixel_data_t);
+    //// gain_level
+    H5::ArrayType gain_level_t(H5::PredType::NATIVE_UINT8, 3, frame_dim);
+    insertMember("gain_level", HOFFSET(Field, gain_level), gain_level_t);
+    //// late_arrived
+    insertMember("late_arrived", HOFFSET(Field, late_arrived), H5::PredType::NATIVE_HBOOL);
+    //// calib_level
+    insertMember("calib_level", HOFFSET(Field, calib_level), H5::PredType::NATIVE_INT8);
+}
+
+diffraflow::ImageDataType::~ImageDataType() {}
+
+bool diffraflow::ImageDataType::decode(ImageDataType::Field& image_data, const char* buffer, const size_t len) {
+
+    if (len < 11) return false;
+
+    // bunch_id
+    image_data.bunch_id = gDC.decode_byte<uint64_t>(buffer, 0, 7);
+
+    // alignment
+    uint16_t alignment_bits = gDC.decode_byte<uint16_t>(buffer, 8, 9);
+    for (size_t i = 0; i < MOD_CNT; i++) {
+        image_data.alignment[i] = (1 << (15 - i)) & alignment_bits;
+    }
+
+    // late_arrived
+    image_data.late_arrived = gDC.decode_byte<uint8_t>(buffer, 10, 10);
+
+    // frame data
+    size_t current_pos = 11;
+    for (size_t i = 0; i < MOD_CNT; i++) {
+        if (image_data.alignment[i]) {
+            if (len - current_pos < FRAME_S) return false;
+            const char* frame_buffer = buffer + current_pos;
+            // verification
+            uint32_t header = gDC.decode_byte<uint32_t>(frame_buffer, 0, 3);
+            if (header != 0xDEFAF127) {
+                return false;
+            }
+            uint64_t bunch_id = gDC.decode_byte<uint64_t>(frame_buffer, 12, 19);
+            if (bunch_id != image_data.bunch_id) {
+                return false;
+            }
+            uint16_t module_id = gDC.decode_byte<uint16_t>(frame_buffer, 6, 7);
+            if (module_id != i) {
+                return false;
+            }
+            // cell_id
+            image_data.cell_id[i] = gDC.decode_byte<uint16_t>(frame_buffer, 8, 9);
+            // status
+            image_data.status[i] = gDC.decode_byte<uint16_t>(frame_buffer, 10, 11);
+
+            for (size_t h = 0; h < FRAME_H; h++) {
+                for (size_t w = 0; w < FRAME_W; w++) {
+                    size_t offset = 20 + 2 * (h * FRAME_W + w);
+                    // pixel_data
+                    image_data.gain_level[i][h][w] = gDC.decode_bit<uint8_t>(frame_buffer + offset, 0, 1);
+                    // gain_level
+                    image_data.pixel_data[i][h][w] = gDC.decode_bit<uint16_t>(frame_buffer + offset, 2, 15);
+                }
+            }
+
+            current_pos += FRAME_S;
+        } else {
+            // cell_id
+            image_data.cell_id[i] = 0;
+            // status
+            image_data.status[i] = 0;
+
+            for (size_t h = 0; h < FRAME_H; h++) {
+                for (size_t w = 0; w < FRAME_W; w++) {
+                    // pixel_data
+                    image_data.pixel_data[i][h][w] = 0;
+                    // gain_level
+                    image_data.gain_level[i][h][w] = 0;
+                }
+            }
+        }
+    }
+
+    // calib_level
+    image_data.calib_level = 0;
+
+    return true;
+}
---
+++ diffraflow/common/src/main/public/ImageDataType.hh
@@ -0,0 +1,36 @@
+#ifndef __ImageDataType_H__
+#define __ImageDataType_H__
+
+#include "H5Cpp.h"
+
+#define MOD_CNT 16
+#define FRAME_H 512
+#define FRAME_W 128
+#define FRAME_L 65536
+#define FRAME_S 131096
+
+namespace diffraflow {
+
+    class ImageDataType : public H5::CompType {
+    public:
+        ImageDataType();
+        ~ImageDataType();
+
+    public:
+        struct Field {
+            uint64_t bunch_id;
+            bool alignment[MOD_CNT];
+            int16_t cell_id[MOD_CNT];
+            uint16_t status[MOD_CNT];
+            float pixel_data[MOD_CNT][FRAME_H][FRAME_W];
+            uint8_t gain_level[MOD_CNT][FRAME_H][FRAME_W];
+            bool late_arrived;
+            int8_t calib_level;
+        };
+
+    public:
+        static bool decode(Field& image_data, const char* buffer, const size_t len);
+    };
+} // namespace diffraflow
+
+#endif

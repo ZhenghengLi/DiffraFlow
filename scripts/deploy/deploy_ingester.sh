c971f2ccddeefb06aa454e3ba284508f10a173ee
Zhengheng Li <zhenghenge@gmail.com>
2020-09-03 16:54:27 +0800

    8 ingesters is more stable

--- scripts/deploy/deploy_ingester.sh
+++ scripts/deploy/deploy_ingester.sh
@@ -9,8 +9,8 @@ helm -n diffraflow install -f $value_dir/ingester-01.yaml ingester-01 $chart_dir
 helm -n diffraflow install -f $value_dir/ingester-02.yaml ingester-02 $chart_dir/ingester
 helm -n diffraflow install -f $value_dir/ingester-03.yaml ingester-03 $chart_dir/ingester
 helm -n diffraflow install -f $value_dir/ingester-04.yaml ingester-04 $chart_dir/ingester
-# helm -n diffraflow install -f $value_dir/ingester-05.yaml ingester-05 $chart_dir/ingester
-# helm -n diffraflow install -f $value_dir/ingester-06.yaml ingester-06 $chart_dir/ingester
-# helm -n diffraflow install -f $value_dir/ingester-07.yaml ingester-07 $chart_dir/ingester
-# helm -n diffraflow install -f $value_dir/ingester-08.yaml ingester-08 $chart_dir/ingester
+helm -n diffraflow install -f $value_dir/ingester-05.yaml ingester-05 $chart_dir/ingester
+helm -n diffraflow install -f $value_dir/ingester-06.yaml ingester-06 $chart_dir/ingester
+helm -n diffraflow install -f $value_dir/ingester-07.yaml ingester-07 $chart_dir/ingester
+helm -n diffraflow install -f $value_dir/ingester-08.yaml ingester-08 $chart_dir/ingester
 

cb137b0434ef65ee75c94b769c51b4ccbdef1fbc
Zhengheng Li <zhenghenge@gmail.com>
2020-09-11 13:50:31 +0800

    implement ImageFrameRaw::check_dgrams_integrity()

--- diffraflow/common/src/main/cpp/ImageFrameRaw.cc
+++ diffraflow/common/src/main/cpp/ImageFrameRaw.cc
@@ -1,8 +1,11 @@
 #include "ImageFrameRaw.hh"
 #include "Decoder.hh"
 #include <cstring>
+#include <boost/crc.hpp>
 
 #define FRAME_SIZE 131096
+
+using boost::crc_32_type;
 
 diffraflow::ImageFrameRaw::ImageFrameRaw() {
     module_id = -1;
@@ -54,8 +57,62 @@ bool diffraflow::ImageFrameRaw::add_dgram(shared_ptr<vector<char>>& dgram) {
 }
 
 int diffraflow::ImageFrameRaw::check_dgrams_integrity() {
-    //
-    return -1;
+    // (1) check dgram count
+    if (dgram_list_.size() != 95) {
+        // -1: wrong dgram count
+        return -1;
+    }
+    // (2) check size of each dgram
+    for (size_t i = 0; i < dgram_list_.size(); i++) {
+        if (i == 0) {
+            if (dgram_list_[i]->size() != 1380) {
+                // -2: wrong first dgram size
+                return -2;
+            }
+        } else {
+            if (dgram_list_[i]->size() != 1384) {
+                // -3: wrong other dgram size
+                return -3;
+            }
+        }
+    }
+    // (3) check dgram order
+    uint8_t target_mod_id = 0;
+    uint16_t target_frm_sn = 0;
+    uint8_t previous_seg_sn = 0;
+    for (size_t i = 0; i < dgram_list_.size(); i++) {
+        if (i == 0) {
+            target_mod_id = gDC.decode_byte<uint8_t>(dgram_list_[i]->data(), 0, 0);
+            target_frm_sn = gDC.decode_byte<uint16_t>(dgram_list_[i]->data(), 1, 2);
+            previous_seg_sn = gDC.decode_byte<uint8_t>(dgram_list_[i]->data(), 3, 3);
+        } else {
+            uint8_t current_mod_id = gDC.decode_byte<uint8_t>(dgram_list_[i]->data(), 0, 0);
+            uint16_t current_frm_sn = gDC.decode_byte<uint16_t>(dgram_list_[i]->data(), 1, 2);
+            uint8_t current_seg_sn = gDC.decode_byte<uint8_t>(dgram_list_[i]->data(), 3, 3);
+            if (current_mod_id == target_mod_id && current_frm_sn == target_frm_sn &&
+                current_seg_sn == previous_seg_sn + 1) {
+                previous_seg_sn = current_seg_sn;
+            } else {
+                // -4: found wrong order or mismatched dgrams
+                return -4;
+            }
+        }
+    }
+    // (4) check crc
+    crc_32_type crc_32;
+    for (size_t i = 0; i < dgram_list_.size() - 1; i++) {
+        crc_32.process_bytes(dgram_list_[i]->data() + 4, dgram_list_[i]->size() - 4);
+    }
+    size_t last_index = dgram_list_.size() - 1;
+    size_t last_size = dgram_list_[last_index]->size();
+    crc_32.process_bytes(dgram_list_[last_index]->data() + 4, dgram_list_[last_index]->size() - 8);
+    uint32_t target_crc = gDC.decode_byte<uint32_t>(dgram_list_[last_index]->data(), last_size - 4, last_size - 1);
+    if (target_crc != crc_32.checksum()) {
+        // -5: wrong CRC
+        return -5;
+    }
+    // return total size of payload on success
+    return 131096;
 }
 
 void diffraflow::ImageFrameRaw::sort_dgrams() {

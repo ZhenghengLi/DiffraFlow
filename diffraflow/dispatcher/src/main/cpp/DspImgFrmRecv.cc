85ef20b77826eb0d7be97f1c3eb4c206aaf9dfa8
Zhengheng Li <zhenghenge@gmail.com>
2020-09-11 15:07:39 +0800

    implement :DspImgFrmRecv::process_datagram_()

--- diffraflow/dispatcher/src/main/cpp/DspImgFrmRecv.cc
+++ diffraflow/dispatcher/src/main/cpp/DspImgFrmRecv.cc
@@ -1,4 +1,8 @@
 #include "DspImgFrmRecv.hh"
+#include "ImageFrameRaw.hh"
+#include "Decoder.hh"
+
+using std::make_shared;
 
 log4cxx::LoggerPtr diffraflow::DspImgFrmRecv::logger_ = log4cxx::Logger::getLogger("DspImgFrmRecv");
 
@@ -11,5 +15,30 @@ diffraflow::DspImgFrmRecv::~DspImgFrmRecv() {
 }
 
 void diffraflow::DspImgFrmRecv::process_datagram_(shared_ptr<vector<char>>& datagram) {
-    //
+    if (datagram->size() < 100) {
+        return;
+    }
+    uint8_t dgram_mod_id = gDC.decode_byte<uint8_t>(datagram->data(), 0, 0);
+    if (dgram_mod_id >= MOD_CNT) {
+        return;
+    }
+    shared_ptr<ImageFrameRaw>& image_frame = image_frame_arr[dgram_mod_id];
+    uint16_t dgram_frm_sn = gDC.decode_byte<uint16_t>(datagram->data(), 1, 2);
+    uint8_t dgram_seg_sn = gDC.decode_byte<uint8_t>(datagram->data(), 3, 3);
+    if (dgram_seg_sn == 0) {
+        if (image_frame) {
+            // push image_frame in checking queue
+        }
+        image_frame = make_shared<ImageFrameRaw>();
+        image_frame->add_dgram(datagram);
+    } else {
+        if (image_frame) {
+            if (dgram_frm_sn == image_frame->dgram_frm_sn) {
+                image_frame->add_dgram(datagram);
+            } else {
+                // push image_frame in checking queue
+                image_frame = nullptr;
+            }
+        }
+    }
 }
--- diffraflow/dispatcher/src/main/headers/DspImgFrmRecv.hh
+++ diffraflow/dispatcher/src/main/headers/DspImgFrmRecv.hh
@@ -2,8 +2,16 @@
 #define __DspImgFrmRecv_H__
 
 #include "GenericDgramReceiver.hh"
+#include <memory>
+
+#define MOD_CNT 16
+
+using std::shared_ptr;
 
 namespace diffraflow {
+
+    class ImageFrameRaw;
+
     class DspImgFrmRecv : public GenericDgramReceiver {
     public:
         DspImgFrmRecv(string host, int port);
@@ -13,8 +21,12 @@ namespace diffraflow {
         void process_datagram_(shared_ptr<vector<char>>& datagram) override;
 
     private:
+        shared_ptr<ImageFrameRaw> image_frame_arr[MOD_CNT];
+
+    private:
         static log4cxx::LoggerPtr logger_;
     };
+
 } // namespace diffraflow
 
 #endif

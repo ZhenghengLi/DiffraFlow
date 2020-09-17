e7135a48faf554da1419d9dd4fec256601edb61b
Zhengheng Li <zhenghenge@gmail.com>
2020-09-17 20:58:35 +0800

    GenericDgramReceiver: set rcvbufsize to 64 MiB

--- diffraflow/common/src/main/cpp/GenericDgramReceiver.cc
+++ diffraflow/common/src/main/cpp/GenericDgramReceiver.cc
@@ -59,7 +59,7 @@ bool diffraflow::GenericDgramReceiver::create_udp_sock_() {
     }
 
     // set larger receive buffer
-    int rcvbufsize = 24 * 1024 * 1024; // 24 MiB
+    int rcvbufsize = 64 * 1024 * 1024; // 64 MiB
     setsockopt(receiver_sock_fd_, SOL_SOCKET, SO_RCVBUF, (char*)&rcvbufsize, sizeof(rcvbufsize));
 
     // bind address

822bc43dfb8beea69f2851aa8bcfaf955cd7abc7
Zhengheng Li <zhenghenge@gmail.com>
2020-09-18 10:52:22 +0800

    print_partial_image_index.py: print bunch_id

--- scripts/print_partial_image_index.py
+++ scripts/print_partial_image_index.py
@@ -16,8 +16,9 @@ image_data_dset = h5file['image_data']
 total_entries = image_data_dset.shape[0]
 
 for x in range(total_entries):
+    bunch_id = image_data_dset[x]['bunch_id']
     alignment = image_data_dset[x]['alignment']
     if np.sum(alignment) != 16:
-        print(x, " : ", list(alignment))
+        print(x, ":", bunch_id, ":", list(alignment))
 
 h5file.close()

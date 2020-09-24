83786214e15ec0bdb13a232b7554f7d578e3328e
Zhengheng Li <zhenghenge@gmail.com>
2020-09-24 14:03:08 +0800

    CtrHttpServer: finish request before return for all HTTP methods

--- diffraflow/controller/src/main/cpp/CtrHttpServer.cc
+++ diffraflow/controller/src/main/cpp/CtrHttpServer.cc
@@ -104,10 +104,10 @@ void diffraflow::CtrHttpServer::handleGet_(http_request message) {
         paths_json[2] = json::value::string("/config");
         json::value root_json;
         root_json["paths"] = paths_json;
-        message.reply(status_codes::OK, root_json);
+        message.reply(status_codes::OK, root_json).get();
         return;
     } else if (path_vec.size() > 2) {
-        message.reply(status_codes::NotFound);
+        message.reply(status_codes::NotFound).get();
         return;
     }
 
@@ -116,23 +116,23 @@ void diffraflow::CtrHttpServer::handleGet_(http_request message) {
 
     if (request_type == "event") {
         if (monitor_load_balancer_ == nullptr) {
-            message.reply(status_codes::NotImplemented);
+            message.reply(status_codes::NotImplemented).get();
             return;
         }
         if (!request_value.empty()) {
             if (!regex_match(request_value, number_regex)) {
-                message.reply(status_codes::NotFound);
+                message.reply(status_codes::NotFound).get();
                 return;
             }
         }
         if (monitor_load_balancer_->do_one_request(response, request_value)) {
-            message.reply(response);
+            message.reply(response).get();
         } else {
-            message.reply(status_codes::NotFound);
+            message.reply(status_codes::NotFound).get();
         }
     } else if (request_type == "config") {
         if (zookeeper_config_client_ == nullptr) {
-            message.reply(status_codes::NotImplemented);
+            message.reply(status_codes::NotImplemented).get();
             return;
         }
         if (request_value.empty()) {
@@ -145,9 +145,9 @@ void diffraflow::CtrHttpServer::handleGet_(http_request message) {
                 }
                 json::value root_json;
                 root_json["config_list"] = config_list_json;
-                message.reply(status_codes::OK, root_json);
+                message.reply(status_codes::OK, root_json).get();
             } else {
-                message.reply(status_codes::InternalError);
+                message.reply(status_codes::InternalError).get();
             }
         } else {
             map<string, string> config_map;
@@ -164,35 +164,35 @@ void diffraflow::CtrHttpServer::handleGet_(http_request message) {
                 json::value root_json;
                 root_json["name"] = json::value::string(request_value);
                 root_json["data"] = config_map_json;
-                message.reply(status_codes::OK, root_json);
+                message.reply(status_codes::OK, root_json).get();
             } else if (zoo_err == ZNONODE) {
-                message.reply(status_codes::NotFound);
+                message.reply(status_codes::NotFound).get();
             } else {
-                message.reply(status_codes::InternalError);
+                message.reply(status_codes::InternalError).get();
             }
         }
     } else {
-        message.reply(status_codes::NotFound);
+        message.reply(status_codes::NotFound).get();
     }
 }
 
 void diffraflow::CtrHttpServer::handlePost_(http_request message) {
 
     if (zookeeper_config_client_ == nullptr) {
-        message.reply(status_codes::NotImplemented);
+        message.reply(status_codes::NotImplemented).get();
         return;
     }
 
     vector<utility::string_t> path_vec = uri::split_path(message.relative_uri().path());
     http_response response;
     if (path_vec.size() != 2) {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string request_type = path_vec[0];
     string request_value = path_vec[1];
     if (request_type != "config") {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string znode_path = string("/") + request_value;
@@ -204,41 +204,42 @@ void diffraflow::CtrHttpServer::handlePost_(http_request message) {
             if (iter->second.is_string()) {
                 config_map[iter->first] = iter->second.as_string();
             } else {
-                message.reply(
-                    status_codes::BadRequest, utility::string_t("config value should only be of type string."));
+                message
+                    .reply(status_codes::BadRequest, utility::string_t("config value should only be of type string."))
+                    .get();
                 return;
             }
         }
         int zoo_err = zookeeper_config_client_->zookeeper_create_config(znode_path.c_str(), config_map);
         if (zoo_err == ZOK) {
-            message.reply(status_codes::OK);
+            message.reply(status_codes::OK).get();
         } else if (zoo_err == ZNODEEXISTS) {
-            message.reply(status_codes::Conflict, utility::string_t("config name already exists."));
+            message.reply(status_codes::Conflict, utility::string_t("config name already exists.")).get();
         } else {
-            message.reply(status_codes::InternalError);
+            message.reply(status_codes::InternalError).get();
         }
     } else {
-        message.reply(status_codes::BadRequest, utility::string_t("request body should be a json object."));
+        message.reply(status_codes::BadRequest, utility::string_t("request body should be a json object.")).get();
     }
 }
 
 void diffraflow::CtrHttpServer::handlePut_(http_request message) {
 
     if (zookeeper_config_client_ == nullptr) {
-        message.reply(status_codes::NotImplemented);
+        message.reply(status_codes::NotImplemented).get();
         return;
     }
 
     vector<utility::string_t> path_vec = uri::split_path(message.relative_uri().path());
     http_response response;
     if (path_vec.size() != 2) {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string request_type = path_vec[0];
     string request_value = path_vec[1];
     if (request_type != "config") {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string znode_path = string("/") + request_value;
@@ -250,41 +251,42 @@ void diffraflow::CtrHttpServer::handlePut_(http_request message) {
             if (iter->second.is_string()) {
                 config_map[iter->first] = iter->second.as_string();
             } else {
-                message.reply(
-                    status_codes::BadRequest, utility::string_t("config value should only be of type string."));
+                message
+                    .reply(status_codes::BadRequest, utility::string_t("config value should only be of type string."))
+                    .get();
                 return;
             }
         }
         int zoo_err = zookeeper_config_client_->zookeeper_change_config(znode_path.c_str(), config_map);
         if (zoo_err == ZOK) {
-            message.reply(status_codes::OK);
+            message.reply(status_codes::OK).get();
         } else if (zoo_err == ZNONODE) {
-            message.reply(status_codes::BadRequest, utility::string_t("config name does not exist."));
+            message.reply(status_codes::BadRequest, utility::string_t("config name does not exist.")).get();
         } else {
-            message.reply(status_codes::InternalError);
+            message.reply(status_codes::InternalError).get();
         }
     } else {
-        message.reply(status_codes::BadRequest, utility::string_t("request body should be a json object."));
+        message.reply(status_codes::BadRequest, utility::string_t("request body should be a json object.")).get();
     }
 }
 
 void diffraflow::CtrHttpServer::handlePatch_(http_request message) {
 
     if (zookeeper_config_client_ == nullptr) {
-        message.reply(status_codes::NotImplemented);
+        message.reply(status_codes::NotImplemented).get();
         return;
     }
 
     vector<utility::string_t> path_vec = uri::split_path(message.relative_uri().path());
     http_response response;
     if (path_vec.size() != 2) {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string request_type = path_vec[0];
     string request_value = path_vec[1];
     if (request_type != "config") {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string znode_path = string("/") + request_value;
@@ -296,8 +298,9 @@ void diffraflow::CtrHttpServer::handlePatch_(http_request message) {
             if (iter->second.is_string()) {
                 config_map_patch[iter->first] = iter->second.as_string();
             } else {
-                message.reply(
-                    status_codes::BadRequest, utility::string_t("config value should only be of type string."));
+                message
+                    .reply(status_codes::BadRequest, utility::string_t("config value should only be of type string."))
+                    .get();
                 return;
             }
         }
@@ -308,10 +311,10 @@ void diffraflow::CtrHttpServer::handlePatch_(http_request message) {
         int zoo_err =
             zookeeper_config_client_->zookeeper_fetch_config(znode_path.c_str(), config_map, config_mtime, version);
         if (zoo_err == ZNONODE) {
-            message.reply(status_codes::BadRequest, utility::string_t("config name does not exist."));
+            message.reply(status_codes::BadRequest, utility::string_t("config name does not exist.")).get();
             return;
         } else if (zoo_err != ZOK) {
-            message.reply(status_codes::InternalError);
+            message.reply(status_codes::InternalError).get();
             return;
         }
         // patch
@@ -321,47 +324,47 @@ void diffraflow::CtrHttpServer::handlePatch_(http_request message) {
         // update
         zoo_err = zookeeper_config_client_->zookeeper_change_config(znode_path.c_str(), config_map, version);
         if (zoo_err == ZOK) {
-            message.reply(status_codes::OK);
+            message.reply(status_codes::OK).get();
         } else if (zoo_err == ZNONODE) {
-            message.reply(
-                status_codes::InternalError, utility::string_t("config name may be deleted during patching."));
+            message.reply(status_codes::InternalError, utility::string_t("config name may be deleted during patching."))
+                .get();
         } else if (zoo_err == ZBADVERSION) {
-            message.reply(
-                status_codes::InternalError, utility::string_t("config name may be updated during patching."));
+            message.reply(status_codes::InternalError, utility::string_t("config name may be updated during patching."))
+                .get();
         } else {
-            message.reply(status_codes::InternalError);
+            message.reply(status_codes::InternalError).get();
         }
     } else {
-        message.reply(status_codes::BadRequest, utility::string_t("request body should be a json object."));
+        message.reply(status_codes::BadRequest, utility::string_t("request body should be a json object.")).get();
     }
 }
 
 void diffraflow::CtrHttpServer::handleDel_(http_request message) {
 
     if (zookeeper_config_client_ == nullptr) {
-        message.reply(status_codes::NotImplemented);
+        message.reply(status_codes::NotImplemented).get();
         return;
     }
 
     vector<utility::string_t> path_vec = uri::split_path(message.relative_uri().path());
     http_response response;
     if (path_vec.size() != 2) {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string request_type = path_vec[0];
     string request_value = path_vec[1];
     if (request_type != "config") {
-        message.reply(status_codes::BadRequest);
+        message.reply(status_codes::BadRequest).get();
         return;
     }
     string znode_path = string("/") + request_value;
     int zoo_err = zookeeper_config_client_->zookeeper_delete_config(znode_path.c_str());
     if (zoo_err == ZOK) {
-        message.reply(status_codes::OK);
+        message.reply(status_codes::OK).get();
     } else if (zoo_err == ZNONODE) {
-        message.reply(status_codes::BadRequest, utility::string_t("config name does not exist."));
+        message.reply(status_codes::BadRequest, utility::string_t("config name does not exist.")).get();
     } else {
-        message.reply(status_codes::InternalError);
+        message.reply(status_codes::InternalError).get();
     }
 }

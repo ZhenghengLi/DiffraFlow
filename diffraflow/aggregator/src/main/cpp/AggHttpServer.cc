4fbc012d4edaacc25a5e4d688d2fb18779521d0e
Zhengheng Li <zhenghenge@gmail.com>
2020-09-29 13:27:30 +0800

    aggregator: implemented AggHttpServer

--- diffraflow/aggregator/src/main/cpp/AggHttpServer.cc
+++ diffraflow/aggregator/src/main/cpp/AggHttpServer.cc
@@ -1,9 +1,74 @@
 #include "AggHttpServer.hh"
+#include "AggMetrics.hh"
 
-diffraflow::AggHttpServer::AggHttpServer() {
-    //
+using namespace web;
+using namespace http;
+using namespace experimental::listener;
+using std::lock_guard;
+using std::unique_lock;
+
+log4cxx::LoggerPtr diffraflow::AggHttpServer::logger_ = log4cxx::Logger::getLogger("AggHttpServer");
+
+diffraflow::AggHttpServer::AggHttpServer(AggMetrics* metrics) {
+    aggregated_metrics_ = metrics;
+    server_status_ = kNotStart;
 }
 
 diffraflow::AggHttpServer::~AggHttpServer() {
     //
+}
+
+bool diffraflow::AggHttpServer::start(string host, int port) {
+    if (server_status_ == kRunning) {
+        LOG4CXX_WARN(logger_, "http server has already been started.");
+        return false;
+    }
+
+    uri_builder uri_b;
+    uri_b.set_scheme("http");
+    uri_b.set_host(host);
+    uri_b.set_port(port);
+    listener_ = new http_listener(uri_b.to_uri());
+    listener_->support(methods::GET, std::bind(&AggHttpServer::handleGet_, this, std::placeholders::_1));
+
+    try {
+        listener_->open().wait();
+        server_status_ = kRunning;
+        return true;
+    } catch (std::exception& e) {
+        LOG4CXX_ERROR(logger_, "failed to start http server: " << e.what());
+        return false;
+    } catch (...) {
+        LOG4CXX_ERROR(logger_, "failed to start http server with unknown error.");
+        return false;
+    }
+    return true;
+}
+
+void diffraflow::AggHttpServer::stop() {
+    if (listener_ == nullptr) return;
+
+    try {
+        listener_->close().wait();
+    } catch (std::exception& e) {
+        LOG4CXX_WARN(logger_, "exception found when closing http listener: " << e.what());
+    } catch (...) {
+        LOG4CXX_WARN(logger_, "an unknown exception found when closing http listener.");
+    }
+
+    delete listener_;
+    listener_ = nullptr;
+
+    server_status_ = kStopped;
+    cv_status_.notify_all();
+}
+
+void diffraflow::AggHttpServer::wait() {
+    unique_lock<mutex> ulk(mtx_status_);
+    cv_status_.wait(ulk, [this]() { return server_status_ != kRunning; });
+}
+
+void diffraflow::AggHttpServer::handleGet_(http_request message) {
+    //
+    message.reply(status_codes::NotFound).get();
 }
--- diffraflow/aggregator/src/main/headers/AggHttpServer.hh
+++ diffraflow/aggregator/src/main/headers/AggHttpServer.hh
@@ -1,11 +1,54 @@
 #ifndef __AggHttpServer_H__
 #define __AggHttpServer_H__
 
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <cpprest/http_listener.h>
+#include <cpprest/http_client.h>
+#include <pplx/pplxtasks.h>
+#include <log4cxx/logger.h>
+
+using std::string;
+using web::http::experimental::listener::http_listener;
+using web::http::http_request;
+using web::http::client::http_client;
+using web::http::client::http_client_config;
+using std::atomic;
+using std::mutex;
+using std::condition_variable;
+using std::vector;
+
 namespace diffraflow {
+
+    class AggMetrics;
+
     class AggHttpServer {
     public:
-        AggHttpServer();
+        explicit AggHttpServer(AggMetrics* metrics);
         ~AggHttpServer();
+
+        bool start(string host, int port);
+        void stop();
+        void wait();
+
+    public:
+        enum WorkerStatus { kNotStart, kRunning, kStopped };
+
+    private:
+        http_listener* listener_;
+
+        atomic<WorkerStatus> server_status_;
+        mutex mtx_status_;
+        condition_variable cv_status_;
+
+        AggMetrics* aggregated_metrics_;
+
+    private:
+        void handleGet_(http_request message);
+
+    private:
+        static log4cxx::LoggerPtr logger_;
     };
 } // namespace diffraflow
 

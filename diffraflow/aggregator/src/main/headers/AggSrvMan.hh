a4d5ae733f00f1459cd40992cf5228e4c212be60
Zhengheng Li <zhenghenge@gmail.com>
2020-09-29 14:54:26 +0800

    aggregator: metrics_reporter_

--- diffraflow/aggregator/src/main/headers/AggSrvMan.hh
+++ diffraflow/aggregator/src/main/headers/AggSrvMan.hh
@@ -4,6 +4,8 @@
 #include <atomic>
 #include <log4cxx/logger.h>
 
+#include "MetricsReporter.hh"
+
 using std::atomic_bool;
 using std::string;
 
@@ -28,6 +30,8 @@ namespace diffraflow {
         AggHttpServer* http_server_;
 
         atomic_bool running_flag_;
+
+        MetricsReporter metrics_reporter_;
 
     private:
         static log4cxx::LoggerPtr logger_;

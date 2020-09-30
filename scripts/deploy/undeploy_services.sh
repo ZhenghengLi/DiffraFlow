84e7cce73789558bc1dcefa0da2a1557d8ac5a1a
Zhengheng Li <zhenghenge@gmail.com>
2020-09-30 12:33:57 +0800

    add scripts to deploy and undeploy services

---
+++ scripts/deploy/undeploy_services.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tmp_dir=$(dirname ${BASH_SOURCE[0]})
+base_dir=$(cd $tmp_dir/../.. > /dev/null ; pwd)
+yaml_dir=$base_dir/deploy/services
+
+kubectl delete -f $yaml_dir/pulsar-broker.yaml
+kubectl delete -f $yaml_dir/pulsar-bookkeeper.yaml
+kubectl delete -f $yaml_dir/zookeeper.yaml
+

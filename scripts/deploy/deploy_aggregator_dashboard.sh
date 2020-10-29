2e058d8ca461b58e2b234d9c75571d759e4eb5b9
Zhengheng Li <zhenghenge@gmail.com>
2020-10-29 19:58:56 +0800

    deploy dashboard

--- scripts/deploy/deploy_aggregator_dashboard.sh
+++ scripts/deploy/deploy_aggregator_dashboard.sh
@@ -6,4 +6,5 @@ chart_dir=$base_dir/deploy/diffraflow/charts
 value_dir=$base_dir/deploy/diffraflow/values
 
 helm -n diffraflow install -f $value_dir/aggregator-1.yaml aggregator-1 $chart_dir/aggregator
+helm -n diffraflow install -f $value_dir/dashboard-1.yaml dashboard-1 $chart_dir/dashboard
 

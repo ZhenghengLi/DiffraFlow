6f6c0721aab45d42e1c548849ca68020efcd0d58
Zhengheng Li <zhenghenge@gmail.com>
2020-11-02 17:04:23 +0800

    config_net.sh

--- scripts/config_net.sh
+++ scripts/config_net.sh
@@ -2,5 +2,5 @@ sysctl -w net.core.rmem_max=67108864
 sysctl -w net.core.rmem_default=33554432
 sysctl -w net.core.wmem_max=67108864
 sysctl -w net.core.wmem_default=33554432
-sysctl -w net.core.netdev_max_backlog=100000
-ifconfig bond0 mtu 9000 txqueuelen 10000 up
+sysctl -w net.core.netdev_max_backlog=500000
+ifconfig bond0 mtu 9000 txqueuelen 20000 up

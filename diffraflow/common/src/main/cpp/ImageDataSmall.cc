ad124fb7f447197a19a99d8384e7d73426c3ccee
Zhengheng Li <zhenghenge@gmail.com>
2020-11-03 08:35:40 +0800

    ImageDataSmall: use fixed energy range

--- diffraflow/common/src/main/cpp/ImageDataSmall.cc
+++ diffraflow/common/src/main/cpp/ImageDataSmall.cc
@@ -20,18 +20,25 @@ diffraflow::ImageDataSmall& diffraflow::ImageDataSmall::operator=(const ImageDat
 void diffraflow::ImageDataSmall::copy_from_(const ImageData& image_data, float energy_down_cut, float energy_up_cut) {
     bunch_id = image_data.bunch_id;
     alignment_vec = image_data.alignment_vec;
-    max_energy = numeric_limits<float>::min();
-    min_energy = numeric_limits<float>::max();
-    for (size_t i = 0; i < image_data.image_frame_vec.size(); i++) {
-        if (!image_data.image_frame_vec[i]) continue;
-        ImageFrame& image_frame = *image_data.image_frame_vec[i];
-        for (size_t j = 0; j < image_frame.pixel_data.size(); j++) {
-            if (image_frame.pixel_data[j] > max_energy) max_energy = image_frame.pixel_data[j];
-            if (image_frame.pixel_data[j] < min_energy) min_energy = image_frame.pixel_data[j];
-        }
-    }
-    if (min_energy < energy_down_cut) min_energy = energy_down_cut;
-    if (max_energy > energy_up_cut) max_energy = energy_up_cut;
+
+    // use dynamic energy range
+    // max_energy = numeric_limits<float>::min();
+    // min_energy = numeric_limits<float>::max();
+    // for (size_t i = 0; i < image_data.image_frame_vec.size(); i++) {
+    //     if (!image_data.image_frame_vec[i]) continue;
+    //     ImageFrame& image_frame = *image_data.image_frame_vec[i];
+    //     for (size_t j = 0; j < image_frame.pixel_data.size(); j++) {
+    //         if (image_frame.pixel_data[j] > max_energy) max_energy = image_frame.pixel_data[j];
+    //         if (image_frame.pixel_data[j] < min_energy) min_energy = image_frame.pixel_data[j];
+    //     }
+    // }
+    // if (min_energy < energy_down_cut) min_energy = energy_down_cut;
+    // if (max_energy > energy_up_cut) max_energy = energy_up_cut;
+
+    // use fixed energy range
+    min_energy = energy_down_cut;
+    max_energy = energy_up_cut;
+
     image_frame_vec.resize(image_data.image_frame_vec.size());
     float gap_energy = max_energy - min_energy;
     for (size_t i = 0; i < image_data.image_frame_vec.size(); i++) {

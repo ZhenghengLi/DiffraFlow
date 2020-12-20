6d91296cbade3a7b99f249f7ebdea6be1b19ec8e
Zhengheng Li <zhenghenge@gmail.com>
2020-12-20 15:43:53 +0800

    update config_net.sh

--- scripts/config_net.sh
+++ scripts/config_net.sh
@@ -3,4 +3,4 @@ sysctl -w net.core.rmem_default=33554432
 sysctl -w net.core.wmem_max=67108864
 sysctl -w net.core.wmem_default=33554432
 sysctl -w net.core.netdev_max_backlog=500000
-ifconfig bond0 mtu 9000 txqueuelen 20000 up
+ifconfig bond0 mtu 9000 txqueuelen 50000 up

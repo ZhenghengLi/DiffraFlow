59735978b002b996bea16a0f3ed59164f6ca3052
Zhengheng Li <zhenghenge@gmail.com>
2021-02-22 14:47:19 +0800

    cmake: add support for CUDA

--- cmake/CommonMacros.cmake
+++ cmake/CommonMacros.cmake
@@ -1,7 +1,14 @@
 function(df_standard_cpp_project proj_name)
-    project(${proj_name})
+    set(options CUDA)
     set(multiValueArgs INCLUDE_DIRS DEFINITIONS LIBRARIES)
-    cmake_parse_arguments(ARG "" "" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(ARG "${options}" "" "${multiValueArgs}" ${ARGN})
+
+    if(ARG_CUDA)
+        project(${proj_name} C CXX CUDA)
+    else()
+        project(${proj_name} C CXX)
+    endif()
+
     include_directories(
         inc
         ${ARG_INCLUDE_DIRS}
@@ -9,19 +16,28 @@ function(df_standard_cpp_project proj_name)
     add_definitions(
         ${ARG_DEFINITIONS}
     )
-    file(GLOB HH_FILES inc/*.hh)
-    file(GLOB CC_FILES src/*.cc)
-    add_executable(${PROJECT_NAME} ${CC_FILES} ${HH_FILES})
+
+    file(GLOB sources src/*)
+    file(GLOB headers inc/*)
+    add_executable(${PROJECT_NAME} ${sources} ${headers})
     target_link_libraries(${PROJECT_NAME}
         ${ARG_LIBRARIES}
     )
+
     install(TARGETS ${PROJECT_NAME} DESTINATION bin)
 endfunction()
 
 function(df_standard_lib_project proj_name)
-    project(${proj_name})
+    set(options CUDA)
     set(multiValueArgs INCLUDE_DIRS DEFINITIONS LIBRARIES)
-    cmake_parse_arguments(ARG "" "" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(ARG "${options}" "" "${multiValueArgs}" ${ARGN})
+
+    if(ARG_CUDA)
+        project(${proj_name} C CXX CUDA)
+    else()
+        project(${proj_name} C CXX)
+    endif()
+
     include_directories(
         inc
         ${ARG_INCLUDE_DIRS}
@@ -29,14 +45,18 @@ function(df_standard_lib_project proj_name)
     add_definitions(
         ${ARG_DEFINITIONS}
     )
-    file(GLOB HH_FILES inc/*.hh)
-    file(GLOB CC_FILES src/*.cc)
-    add_library(${PROJECT_NAME} SHARED ${CC_FILES} ${HH_FILES})
+
+    file(GLOB sources src/*)
+    file(GLOB headers inc/*)
+    add_library(${PROJECT_NAME} SHARED ${sources} ${headers})
     target_link_libraries(${PROJECT_NAME}
         ${ARG_LIBRARIES}
         ${CMAKE_THREAD_LIBS_INIT}
     )
+
     install(TARGETS ${PROJECT_NAME} DESTINATION lib)
+
     set(${PROJECT_NAME}_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/inc CACHE INTERNAL "" FORCE)
     set(${PROJECT_NAME}_DEFINITIONS ${ARG_DEFINITIONS} CACHE INTERNAL "" FORCE)
 endfunction()
+

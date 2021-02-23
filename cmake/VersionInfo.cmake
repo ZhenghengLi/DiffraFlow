5853aef7cd172ceb8ca2a2cd747591ebff6a2920
Zhengheng Li <zhenghenge@gmail.com>
2021-02-23 16:30:08 +0800

    cmake/VersionInfo.cmake: read version from git

--- cmake/VersionInfo.cmake
+++ cmake/VersionInfo.cmake
@@ -1,3 +1,6 @@
+set(DF_VERSION_STRING "v0.2.0")
+set(DF_VERSION_DATE "2021-02-22")
+
 set(DF_COPYRIGHT_STATEMENT "\
  Copyright (c) 2019-2020  G.E.T. department of SHINE project.\\n\
  All Rights Reserved."
@@ -8,10 +11,25 @@ set(DF_MAIN_CONTRIBUTORS "\
  - Zhengheng Li <lizhh1@shanghaitech.edu.cn>"
 )
 
-set(DF_VERSION_STRING "v0.2.0")
-set(DF_VERSION_DATE "2021 Feb 23")
+find_package(Git)
+if(Git_FOUND AND EXISTS ${CMAKE_SOURCE_DIR}/.git)
+    execute_process(COMMAND ${GIT_EXECUTABLE} --git-dir=${CMAKE_SOURCE_DIR}/.git describe --always
+        OUTPUT_VARIABLE git_describe_always
+        RESULT_VARIABLE git_describe_errorcode
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    execute_process(COMMAND ${GIT_EXECUTABLE} --git-dir=${CMAKE_SOURCE_DIR}/.git show -s --format=%cd --date=short
+        OUTPUT_VARIABLE git_show_date
+        RESULT_VARIABLE git_show_errorcode
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    if(NOT git_describe_errorcode AND NOT git_show_errorcode)
+        set(DF_VERSION_STRING ${git_describe_always})
+        set(DF_VERSION_DATE ${git_show_date})
+    endif()
+endif()
 
-function(change_var variable value)
-    set(${variable} ${value} PARENT_SCOPE)
-endfunction()
-
+message("version: ${DF_VERSION_STRING}")
+message("date: ${DF_VERSION_DATE}")

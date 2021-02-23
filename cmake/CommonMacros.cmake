3568f32df71e710794e13e7664d010158155c79d
Zhengheng Li <zhenghenge@gmail.com>
2021-02-23 14:20:43 +0800

    cmake/CommonMacros.cmake: remove project command from functions

--- cmake/CommonMacros.cmake
+++ cmake/CommonMacros.cmake
@@ -1,46 +1,42 @@
-function(df_standard_cpp_project proj_name)
+function(df_standard_cpp target_name)
     set(multiValueArgs INCLUDE_DIRS DEFINITIONS LIBRARIES)
     cmake_parse_arguments(ARG "" "" "${multiValueArgs}" ${ARGN})
 
-    project(${proj_name})
-
     file(GLOB sources src/*)
     file(GLOB headers inc/*)
-    add_executable(${PROJECT_NAME} ${sources} ${headers})
+    add_executable(${target_name} ${sources} ${headers})
 
-    target_link_libraries(${PROJECT_NAME}
+    target_link_libraries(${target_name}
         ${ARG_LIBRARIES}
     )
-    target_include_directories(${PROJECT_NAME} PRIVATE inc ${ARG_INCLUDE_DIRS})
-    target_compile_definitions(${PROJECT_NAME} PRIVATE ${ARG_DEFINITIONS})
+    target_include_directories(${target_name} PRIVATE inc ${ARG_INCLUDE_DIRS})
+    target_compile_definitions(${target_name} PRIVATE ${ARG_DEFINITIONS})
 
-    install(TARGETS ${PROJECT_NAME} DESTINATION bin)
+    install(TARGETS ${target_name} DESTINATION bin)
 endfunction()
 
-function(df_standard_lib_project proj_name)
+function(df_standard_lib target_name)
     set(oneValueArgs ALIAS)
     set(multiValueArgs INCLUDE_DIRS DEFINITIONS LIBRARIES)
     cmake_parse_arguments(ARG "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    project(${proj_name})
-
     file(GLOB sources src/*)
     file(GLOB headers inc/*)
     file(GLOB publics pub/*)
-    add_library(${PROJECT_NAME} SHARED ${sources} ${headers} ${publics})
+    add_library(${target_name} SHARED ${sources} ${headers} ${publics})
 
-    target_link_libraries(${PROJECT_NAME}
+    target_link_libraries(${target_name}
         ${ARG_LIBRARIES}
         ${CMAKE_THREAD_LIBS_INIT}
     )
-    target_include_directories(${PROJECT_NAME} PUBLIC  pub)
-    target_include_directories(${PROJECT_NAME} PRIVATE inc ${ARG_INCLUDE_DIRS})
-    target_compile_definitions(${PROJECT_NAME} PRIVATE ${ARG_DEFINITIONS})
+    target_include_directories(${target_name} PUBLIC  pub)
+    target_include_directories(${target_name} PRIVATE inc ${ARG_INCLUDE_DIRS})
+    target_compile_definitions(${target_name} PRIVATE ${ARG_DEFINITIONS})
 
     if(DEFINED ARG_ALIAS)
-        add_library(${ARG_ALIAS} ALIAS ${PROJECT_NAME})
+        add_library(${ARG_ALIAS} ALIAS ${target_name})
     endif()
 
-    install(TARGETS ${PROJECT_NAME} DESTINATION lib)
+    install(TARGETS ${target_name} DESTINATION lib)
 endfunction()
 

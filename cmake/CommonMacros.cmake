d11facb8079628309ac97231748e4005cc23e0ee
Zhengheng Li <zhenghenge@gmail.com>
2021-02-25 18:14:47 +0800

    cmake/CommonMacros.cmake

--- cmake/CommonMacros.cmake
+++ cmake/CommonMacros.cmake
@@ -6,11 +6,9 @@ function(df_standard_exe target_name)
     file(GLOB headers inc/*)
     add_executable(${target_name} ${sources} ${headers})
 
-    target_link_libraries(${target_name}
-        ${ARG_LIBRARIES}
-    )
     target_include_directories(${target_name} PRIVATE inc ${ARG_INCLUDE_DIRS})
     target_compile_definitions(${target_name} PRIVATE ${ARG_DEFINITIONS})
+    target_link_libraries(${target_name} PRIVATE ${ARG_LIBRARIES})
 
     install(TARGETS ${target_name} DESTINATION bin)
 endfunction()
@@ -25,13 +23,10 @@ function(df_standard_lib target_name)
     file(GLOB publics pub/*)
     add_library(${target_name} SHARED ${sources} ${headers} ${publics})
 
-    target_link_libraries(${target_name}
-        ${ARG_LIBRARIES}
-        ${CMAKE_THREAD_LIBS_INIT}
-    )
     target_include_directories(${target_name} PUBLIC  pub)
     target_include_directories(${target_name} PRIVATE inc ${ARG_INCLUDE_DIRS})
     target_compile_definitions(${target_name} PRIVATE ${ARG_DEFINITIONS})
+    target_link_libraries(${target_name} PUBLIC ${ARG_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
 
     if(DEFINED ARG_ALIAS)
         add_library(${ARG_ALIAS} ALIAS ${target_name})

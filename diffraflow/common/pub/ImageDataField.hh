31cb7158c1bdc10d9faf227d01703ad41978718b
Zhengheng Li <zhenghenge@gmail.com>
2021-03-10 14:13:10 +0800

    ImageDataField: move image data block forward

--- diffraflow/common/pub/ImageDataField.hh
+++ diffraflow/common/pub/ImageDataField.hh
@@ -9,12 +9,12 @@
 
 namespace diffraflow {
     struct ImageDataField {
+        float pixel_data[MOD_CNT][FRAME_H][FRAME_W];
+        unsigned char gain_level[MOD_CNT][FRAME_H][FRAME_W];
         unsigned long bunch_id;
         bool alignment[MOD_CNT];
         short cell_id[MOD_CNT];
         unsigned short status[MOD_CNT];
-        float pixel_data[MOD_CNT][FRAME_H][FRAME_W];
-        unsigned char gain_level[MOD_CNT][FRAME_H][FRAME_W];
         bool late_arrived;
         char calib_level;
     };

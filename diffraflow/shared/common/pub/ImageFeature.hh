fb583c7b82d189364bc0868217880251d87e4e0c
Zhengheng Li <zhenghenge@gmail.com>
2021-03-18 15:58:32 +0800

    ImageFeature.hh: remove the check for __CUDACC__

--- diffraflow/shared/common/pub/ImageFeature.hh
+++ diffraflow/shared/common/pub/ImageFeature.hh
@@ -1,17 +1,13 @@
 #ifndef __ImageFeature_H__
 #define __ImageFeature_H__
 
-#ifndef __CUDACC__
 #include <msgpack.hpp>
-#endif
 
 namespace diffraflow {
     struct ImageFeature {
         float global_rms;
         int peak_counts;
-#ifndef __CUDACC__
         MSGPACK_DEFINE_MAP(global_rms, peak_counts);
-#endif
     };
 } // namespace diffraflow
 

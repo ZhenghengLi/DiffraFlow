ce3d5844d17be2fca380c085afcf54d4695c0d0d
Zhengheng Li <zhenghenge@gmail.com>
2021-03-18 17:02:00 +0800

    add cudatools in utils

---
+++ diffraflow/shared/utils/pub/cudatools.hh
@@ -0,0 +1,14 @@
+#ifndef __cudatools_h__
+#define __cudatools_h__
+
+#include <string>
+
+namespace diffraflow {
+    namespace utils {
+
+        std::string uuid_to_string(char uuid_bytes[16]);
+
+    } // namespace utils
+} // namespace diffraflow
+
+#endif
---
+++ diffraflow/shared/utils/src/cudatools.cc
@@ -0,0 +1,15 @@
+#include "cudatools.hh"
+#include <sstream>
+
+using std::string;
+using std::stringstream;
+
+string diffraflow::utils::uuid_to_string(char uuid_bytes[16]) {
+    stringstream ss;
+    ss << std::hex;
+    for (int i = 0; i < 16; i++) {
+        if (i == 4 || i == 6 || i == 8 || i == 10) ss << "-";
+        ss << (int)((uint8_t*)uuid_bytes)[i];
+    }
+    return ss.str();
+}

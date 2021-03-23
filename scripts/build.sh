b56fc7a69adb0cfa162a16cf6609d6cb1f6757e1
Zhengheng Li <zhenghenge@gmail.com>
2021-03-23 13:52:54 +0800

    scripts/build.s

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,6 @@ base_dir=$(cd $tmp_dir/.. > /dev/null ; pwd)
 registry_url="10.15.86.19:25443"
 
 tag_name="$registry_url/diffraflow"
+docker image rm $tag_name
 docker build $base_dir -t $tag_name && docker push $tag_name
 

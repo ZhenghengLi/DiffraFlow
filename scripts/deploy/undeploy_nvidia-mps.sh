e40a0ba2d1f9e8cc92c3bded9ef30e79486ce545
Zhengheng Li <zhenghenge@gmail.com>
2021-03-26 08:46:19 +0800

    add script for nvidia-mps deployment

---
+++ scripts/deploy/undeploy_nvidia-mps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tmp_dir=$(dirname ${BASH_SOURCE[0]})
+base_dir=$(cd $tmp_dir/../.. > /dev/null ; pwd)
+yaml_dir=$base_dir/deploy/services
+
+kubectl delete -f $yaml_dir/nvidia-mps.yaml
+

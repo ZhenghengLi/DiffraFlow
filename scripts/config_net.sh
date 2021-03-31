9fb35aac8df8c70a6473cbda2eb4a3d16001506e
Zhengheng Li <zhenghenge@gmail.com>
2021-03-31 19:12:45 +0800

    change udp socket buffer size

--- scripts/config_net.sh
+++ scripts/config_net.sh
@@ -1,6 +1,6 @@
-sysctl -w net.core.rmem_max=268435456
+sysctl -w net.core.rmem_max=134217728
 sysctl -w net.core.rmem_default=33554432
-sysctl -w net.core.wmem_max=268435456
+sysctl -w net.core.wmem_max=134217728
 sysctl -w net.core.wmem_default=33554432
-sysctl -w net.core.netdev_max_backlog=2000000
-ifconfig bond0 mtu 9000 txqueuelen 200000 up
+sysctl -w net.core.netdev_max_backlog=4000000
+ifconfig bond0 mtu 9000 txqueuelen 400000 up

bc5c8f4cc03764684a7442e65ca736fc13298475
Zhengheng Li <zhenghenge@gmail.com>
2021-04-02 15:12:15 +0800

    update config_net.sh

--- scripts/config_net.sh
+++ scripts/config_net.sh
@@ -2,5 +2,5 @@ sysctl -w net.core.rmem_max=134217728
 sysctl -w net.core.rmem_default=33554432
 sysctl -w net.core.wmem_max=134217728
 sysctl -w net.core.wmem_default=33554432
-sysctl -w net.core.netdev_max_backlog=4000000
-ifconfig bond0 mtu 9000 txqueuelen 400000 up
+sysctl -w net.core.netdev_max_backlog=5000000
+ifconfig bond0 mtu 9000 txqueuelen 500000 up

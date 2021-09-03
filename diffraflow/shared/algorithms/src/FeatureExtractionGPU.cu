f4962b2a0ff7dae25c2ed05a0313221dcdf25e06
Zhengheng Li <zhenghenge@gmail.com>
2021-09-03 13:18:28 +0800

    FeatureExtraction::global_mean_rms_gpu: apply energy cut

--- diffraflow/shared/algorithms/src/FeatureExtractionGPU.cu
+++ diffraflow/shared/algorithms/src/FeatureExtractionGPU.cu
@@ -1,7 +1,7 @@
 #include "FeatureExtraction.hh"
 
-__global__ void energy_sum_kernel(
-    diffraflow::ImageDataField* image_data_device, double* sum_device, int* count_device) {
+__global__ void energy_sum_kernel(diffraflow::ImageDataField* image_data_device, double* sum_device, int* count_device,
+    float min_energy, float max_energy) {
     int mod = blockIdx.x;  // module
     int row = threadIdx.x; // ASIC row
     int col = threadIdx.y; // ASIC column
@@ -13,8 +13,11 @@ __global__ void energy_sum_kernel(
 
     for (int h_asic = 1; h_asic < 63; h_asic++) {
         for (int w_asic = 0; w_asic < 64; w_asic++) {
-            sum_local += image_data_device->pixel_data[mod][row * 64 + h_asic][col * 64 + w_asic];
-            count_local++;
+            float energy = image_data_device->pixel_data[mod][row * 64 + h_asic][col * 64 + w_asic];
+            if (energy >= min_energy && energy < max_energy) {
+                sum_local += energy;
+                count_local++;
+            }
         }
     }
 
@@ -22,8 +25,8 @@ __global__ void energy_sum_kernel(
     atomicAdd(count_device, count_local);
 }
 
-__global__ void mean_square_sum_kernel(
-    float mean, diffraflow::ImageDataField* image_data_device, double* sum_device, int* count_device) {
+__global__ void mean_square_sum_kernel(float mean, diffraflow::ImageDataField* image_data_device, double* sum_device,
+    int* count_device, float min_energy, float max_energy) {
     int mod = blockIdx.x;  // module
     int row = threadIdx.x; // ASIC row
     int col = threadIdx.y; // ASIC column
@@ -35,9 +38,12 @@ __global__ void mean_square_sum_kernel(
 
     for (int h_asic = 1; h_asic < 63; h_asic++) {
         for (int w_asic = 0; w_asic < 64; w_asic++) {
-            double residual = image_data_device->pixel_data[mod][row * 64 + h_asic][col * 64 + w_asic] - mean;
-            sum_local += residual * residual;
-            count_local++;
+            float energy = image_data_device->pixel_data[mod][row * 64 + h_asic][col * 64 + w_asic];
+            if (energy >= min_energy && energy < max_energy) {
+                double residual = energy - mean;
+                sum_local += residual * residual;
+                count_local++;
+            }
         }
     }
 
@@ -62,12 +68,14 @@ void diffraflow::FeatureExtraction::global_mean_rms_gpu(cudaStream_t stream, dou
     int count_host = 0;
     cudaMemcpyAsync(count_device, &count_host, sizeof(int), cudaMemcpyHostToDevice, stream);
     // mean
-    energy_sum_kernel<<<MOD_CNT, dim3(8, 2), 0, stream>>>(image_data_device, sum_device, count_device);
+    energy_sum_kernel<<<MOD_CNT, dim3(8, 2), 0, stream>>>(
+        image_data_device, sum_device, count_device, min_energy, max_energy);
     divide_init_kernel<<<1, 1, 0, stream>>>(&image_feature_device->global_mean, sum_device, count_device);
     float mean = 0;
     cudaMemcpyAsync(&mean, &image_feature_device->global_mean, sizeof(float), cudaMemcpyDeviceToHost, stream);
     // rms
-    mean_square_sum_kernel<<<MOD_CNT, dim3(8, 2), 0, stream>>>(mean, image_data_device, sum_device, count_device);
+    mean_square_sum_kernel<<<MOD_CNT, dim3(8, 2), 0, stream>>>(
+        mean, image_data_device, sum_device, count_device, min_energy, max_energy);
     divide_init_kernel<<<1, 1, 0, stream>>>(&image_feature_device->global_rms, sum_device, count_device);
     // wait finish
     cudaStreamSynchronize(stream);
